ed06b31036f67639bbb7aebb20cc9bda7aef77c3
chrisohiou57 <chrisohiou57@gmail.com>
2020-10-30 11:26:57 -0400

    Initial Commit

---
+++ src/test/java/com/chrias/BasicRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chrias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
